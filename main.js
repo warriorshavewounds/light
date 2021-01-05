31c9c269bced09bde57b8f0f8829af1a90de7212
Emmanuel Yaw Amoako <nanaamoako919@gmail.com>
2021-01-05 13:32:24 +0000

    second commit
    update of site

---
+++ main.js
@@ -0,0 +1,25 @@
+let fortunes = ['It is certain.',
+  'It is decidedly so.',
+  'Reply hazy try again.',
+  'Cannot predict now.',
+  'My sources say yes.'
+]
+
+let button = document.getElementById('fortuneButton');
+let fortune = document.getElementById('fortune');
+
+function fortuneSelector(){
+  let randomFortune = Math.floor(Math.random() * fortunes.length);
+  return fortunes[randomFortune];
+}
+
+function showFortune(){
+  fortune.innerHTML = fortuneSelector();
+  button.innerHTML = 'Thank you!';
+  button.style.cursor = 'default';
+
+  //add your code here
+
+}
+
+button.addEventListener('click', showFortune);
